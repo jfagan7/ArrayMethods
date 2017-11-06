3f38e4e5a0b7e864770f4169794ccbae32446d62
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-11-06 13:16:52 -0500

    ArrayMethods

--- Lab3.1/src/ArrayMethods.java
+++ Lab3.1/src/ArrayMethods.java
@@ -3,10 +3,12 @@ public class ArrayMethods {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[]list= {1,1,2,3,4,5,6};
+		removeDuplicates(list);
 	}
 	public static int[] removeDuplicates(int[] list)
 	{
+		int size=list.length;
 		int[] newList = null;
 		for(int i=0;i<list.length-1;i++)
 		{
