61ad5247a4fc703cd0294cae633b1371d52cb3a8
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-11-09 12:44:08 -0500

    error

--- Lab3.1/src/ArrayMethods.java
+++ Lab3.1/src/ArrayMethods.java
@@ -4,12 +4,12 @@ public class ArrayMethods {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[]list= {1,1,2,3,4,5,6};
-		removeDuplicates(list);
+		System.out.print(removeDuplicates(list));
 	}
 	public static int[] removeDuplicates(int[] list)
 	{
 		int size=list.length;
-		int[] newList = null;
+		int[] newList = new int[size];
 		for(int i=0;i<list.length-1;i++)
 		{
 			if(list[i]!=list[i+1])
@@ -18,5 +18,9 @@ public class ArrayMethods {
 			}
 		}
 		return newList;
+	}
+	public static void printArr(int[]list))
+	{
+		for(int)
 	}
 }
